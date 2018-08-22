237cc7111d6fbf9a22098521ca7211555622a79d
Jan Joneš <jjones@outlook.cz>
2018-08-22 21:14:32 +0200

    Removed `GtmPackage`.

--- src/GtmExtension/GtmListener.cs
+++ src/GtmExtension/GtmListener.cs
@@ -11,80 +11,10 @@ using Microsoft.VisualStudio.Utilities;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-using System.Threading;
 using Process = System.Diagnostics.Process;
-using Task = System.Threading.Tasks.Task;
 
 namespace GtmExtension
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [Guid(PackageGuidString)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)] // Load the extension when a solution is open.
-    public sealed class GtmPackage : AsyncPackage
-    {
-        /// <summary>
-        /// GtmPackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "208330cb-08c8-4105-b9b2-8f010fbeaf49";
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GtmPackage"/> class.
-        /// </summary>
-        public GtmPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-        }
-
-        #endregion
-    }
-
-    [Export(typeof(IVsTextViewCreationListener))]
-    [ContentType("text")]
-    [TextViewRole(PredefinedTextViewRoles.Editable)]
-    public sealed class AnotherListener : IVsTextViewCreationListener
-    {
-        public void VsTextViewCreated(IVsTextView textViewAdapter)
-        {
-        }
-    }
-
     [Export(typeof(IVsTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Editable)]
@@ -102,8 +32,10 @@ namespace GtmExtension
         private static readonly TimeSpan updateInterval = TimeSpan.FromSeconds(30.0);
 
         #region Imports
+
         [Import]
         public SVsServiceProvider ServiceProvider { get; set; }
+
         #endregion
 
         #region Helper Functions
