e63bab1ce7602021bb4c6d2ad50694afef892c2c
Jan Joneš <jjones@outlook.cz>
2019-08-02 12:08:22 +0200

    Publish to marketplace

--- scripts/GetVersion.ps1
+++ scripts/GetVersion.ps1
@@ -1,3 +1,3 @@
-[xml]$vsix = Get-Content $PSScriptRoot\..\src\GtmExtension\source.extension.vsixmanifest
+[xml]$vsix = Get-Content "$PSScriptRoot\..\src\GtmExtension\source.extension.vsixmanifest"
 $version = $vsix.PackageManifest.Metadata.Identity.Version
 Write-Host "##vso[task.setvariable variable=vsix.version]$version"
