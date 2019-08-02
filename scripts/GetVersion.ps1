4bcdf0f48226bf3a560d0051ce80e834f11a1d1d
Jan Joneš <jjones@outlook.cz>
2019-08-02 11:32:30 +0200

    Set version correctly

--- scripts/GetVersion.ps1
+++ scripts/GetVersion.ps1
@@ -1,2 +1,3 @@
 [xml]$vsix = Get-Content $PSScriptRoot\..\src\GtmExtension\source.extension.vsixmanifest
-$env:VSIX_VERSION = $vsix.PackageManifest.Metadata.Identity.Version
+$version = $vsix.PackageManifest.Metadata.Identity.Version
+Write-Host "##vso[task.setvariable variable=vsix.version]$version"
