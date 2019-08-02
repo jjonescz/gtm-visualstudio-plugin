cfadbcbeef7e1940b39bd9a65b2ca10f34a577f8
Jan Joneš <jjones@outlook.cz>
2019-08-02 12:47:47 +0200

    Move `vsix-publish.json` to `scripts`

--- scripts/PublishVsix.ps1
+++ scripts/PublishVsix.ps1
@@ -1,3 +1,3 @@
 $VisualStudio = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -format json | ConvertFrom-Json
 $VsixPublisher = Join-Path -Path $VisualStudio.installationPath -ChildPath "VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe" -Resolve
-& $VsixPublisher publish -payload $env:vsix -publishManifest "$PSScriptRoot\..\vsix-publish.json" -personalAccessToken $env:MARKETPLACE_TOKEN
+& $VsixPublisher publish -payload $env:vsix -publishManifest "$PSScriptRoot\vsix-publish.json" -personalAccessToken $env:MARKETPLACE_TOKEN
